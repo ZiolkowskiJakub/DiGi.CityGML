e795f6157dfac5a353a9902e4a28b13fe18923c7
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2026-06-10 21:53:38 +0200

    Documentation added (WIP)

--- DiGi.CityGML/Enums/CityModelParameter.cs
+++ DiGi.CityGML/Enums/CityModelParameter.cs
@@ -5,9 +5,18 @@ using System.ComponentModel;
 namespace DiGi.CityGML.Enums
 {
     [AssociatedTypes(typeof(CityModel)), Description("CityModel Parameter")]
+    /// <summary>
+    /// Parameters applicable to a CityModel.
+    /// </summary>
     public enum CityModelParameter
     {
-        [ParameterProperties("79ab4292-94c1-4d39-b33b-9d80cf7df631", "LOD", "Level Of Detail", Core.Parameter.Enums.AccessType.Read), StringParameterValue()] LOD,
-        [ParameterProperties("fc073811-4f72-4d2b-8fdf-552282425ed6", "Year", "Year", Core.Parameter.Enums.AccessType.Read), IntegerParameterValue()] Year,
+        [ParameterProperties("79ab4292-94c1-4d39-b33b-9d80cf7df631", "LOD", "Level Of Detail", Core.Parameter.Enums.AccessType.Read), StringParameterValue()] /// <summary>
+        /// Level of Detail.
+        /// </summary>
+        LOD,
+        [ParameterProperties("fc073811-4f72-4d2b-8fdf-552282425ed6", "Year", "Year", Core.Parameter.Enums.AccessType.Read), IntegerParameterValue()] /// <summary>
+        /// Model year.
+        /// </summary>
+        Year,
     }
 }
