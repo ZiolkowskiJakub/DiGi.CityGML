9152fb6c279f2e07c415d1c925cc82fb9324fb7f
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2026-07-21 11:48:22 +0200

    Code cleaning

--- DiGi.CityGML/Query/RunAsync.cs
+++ DiGi.CityGML/Query/RunAsync.cs
@@ -1,7 +1,6 @@
 using DiGi.CityGML.Classes;
 using DiGi.CityGML.Enums;
 using System;
-using System.Collections.Generic;
 using System.IO;
 using System.IO.Compression;
 using System.Threading.Tasks;
